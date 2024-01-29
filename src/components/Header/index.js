360910d73002789bbcfc2cbfa422b059fb93fe31
imran <mdimrankhan175@gmail.com>
2024-01-30 00:01:55 +0530

    header icons updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -35,12 +35,12 @@ const Header = () => {
      
      <nav className="nav-container">
         <ul className="nav-list">
-          <li><Link to="/ownership" className="nav-link"><FaTasks  style={{ fontSize: '2em', color: '#e67e22' }}  /></Link></li>
-          <li><Link to="/meeting" className="nav-link"><FaCalendar style={{ fontSize: '2em', color: '#2ecc71' }}  /></Link></li>
-          <li><Link to="/emp_section" className="nav-link"><FaUser style={{ fontSize: '2em', color: '#e74c3c' }}   /></Link></li>
-          <li><Link to="/server" className="nav-link"><FaServer style={{ fontSize: '2em', color: '#9b59b6' }}  /></Link></li>
-          <li><Link to="/contact_us" className="nav-link"><FaEnvelope style={{ fontSize: '2em', color: '#e67e22' }}  /></Link></li>
-          <li><Link to="/follow_us" className="nav-link"><FaPhone style={{ fontSize: '2em',  color: '#3498db' }} /></Link></li>
+          <li><Link to="/ownership" className="nav-link"><FaTasks  style={{ fontSize: '1em', color: '#e67e22' }}  /></Link></li>
+          <li><Link to="/meeting" className="nav-link"><FaCalendar style={{ fontSize: '1em', color: '#2ecc71' }}  /></Link></li>
+          <li><Link to="/emp_section" className="nav-link"><FaUser style={{ fontSize: '1em', color: '#e74c3c' }}   /></Link></li>
+          <li><Link to="/server" className="nav-link"><FaServer style={{ fontSize: '1em', color: '#9b59b6' }}  /></Link></li>
+          <li><Link to="/contact_us" className="nav-link"><FaEnvelope style={{ fontSize: '1em', color: '#e67e22' }}  /></Link></li>
+          <li><Link to="/follow_us" className="nav-link"><FaPhone style={{ fontSize: '1em',  color: '#3498db' }} /></Link></li>
         </ul>
       </nav>
     </header>
